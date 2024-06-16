34f157346c6412d2cafd47272975184e616bd141
Davit Tavadze <davit@techpheonix.dev>
2024-06-16 15:14:31 +0400

    added little padding to the output container

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -11,7 +11,7 @@ export const Output = () => {
         Output
       </Badge>
       <div className='flex-1 overflow-auto'>
-        <pre className='whitespace-pre-wrap break-all'>
+        <pre className='whitespace-pre-wrap break-all p-2'>
           {apiResponse
             ? JSON.stringify(apiResponse, null, 2)
             : 'No response yet'}
